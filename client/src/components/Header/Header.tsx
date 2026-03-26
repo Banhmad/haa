61cd463402231bbd1a8187e40e3fdcebf0dac1ba
Banhmad <tamssloht16@gmail.com>
2026-03-26 12:07:14 +0100

    Creating Header component for the project

---
+++ client/src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header: React.FC = () => {
+    return (
+        <header>
+            <h1>Welcome to the Banhmad's Project!</h1>
+        </header>
+    );
+};
+
+export default Header;
